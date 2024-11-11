1d107d8c706ec8416aaf2c4ef3fb78c47864fa24
Yao Wang <yaowang0625@gmail.com>
2024-11-11 14:29:59 -0800

    11/1

---
+++ src/main/java/array/InsertInterval.java
@@ -0,0 +1,36 @@
+package dropbox;
+
+
+import java.util.List;
+
+public class InsertInterval {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> ans = new ArrayList<int[]>();
+        int start = newInterval[0];
+        int end = newInterval[1];
+        int i = 0;
+
+        while(i<intervals.length && intervals[i][1]<start) {
+            ans.add(intervals[i]);
+            i++;
+        }
+
+        while(i<intervals.length && intervals[i][0]<=end) {
+            start = Math.min(start, intervals[i][0]);
+            end = Math.max(end, intervals[i][1]);
+            i++;
+        }
+
+        ans.add(new int[]{start, end});
+
+        while(i<intervals.length){
+            ans.add(intervals[i]);
+            i++;
+        }
+
+
+        return ans.toArray(int[][]::new);
+    }
+}
+
+
--- src/main/java/ninechapter/twopointers/related/LinkedListCycle.java
+++
@@ -1,4 +0,0 @@
-package ninechapter.twopointers.related;
-
-public class LinkedListCycle {
-}
